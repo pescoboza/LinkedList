86618cef9add8c73fa54a28ac3586934b8714579
Pedro Escoboza <lolo23jhon@gmail.com>
2020-09-24 22:00:38 -0700

    :art: Changed list implementation to use a template.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,95 +1,118 @@
-#include "LinkedList.hpp"
-
-LinkedList::Node::Node(int value) : m_data{ value }, m_next{ nullptr }{}
-
-LinkedList::NodePtr& LinkedList::getLast(){
-	NodePtr* it{&m_head};
-	while ((*it)->m_next) {
-		it = &(*it)->m_next;
-	}
-	return *it;
-}
-
-LinkedList::LinkedList() : m_head{ nullptr }, m_tail{ nullptr }{}
-
-size_t LinkedList::size() const {
-	if (!m_head) return 0U;
-	size_t s{ 1U };
-	const NodePtr* node{ &m_head };
-	while ((*node)->m_next) {
-		node = &(*node)->m_next;
-		s++;
-	}
-	return s;
-}
-
-int LinkedList::at(size_t n) const {
-	const NodePtr* node{ &m_head };
-	for (size_t i{ 0U }; i < n; i++) {
-		if (!node) throw std::out_of_range{ "Index out of range." };
-		node = &(*node)->m_next;
-	}
-	
-	return (*node)->m_data;
-}
-
-int& LinkedList::at(size_t n) {
-	NodePtr* node{ &m_head };
-	for (size_t i{ 0U }; i < n; i++) {
-		if (!node) throw std::out_of_range{ "Index out of range." };
-		node = &(*node)->m_next;
-	}
-
-	return (*node)->m_data;
-}
-
-int LinkedList::operator[](size_t n) const {
-	return at(n);
-}
-
-int& LinkedList::operator[](size_t n) {
-	return at(n);
-}
-
-int LinkedList::back() const{
-	return m_tail->m_data;
-}
-
-int& LinkedList::back(){
-	return m_tail->m_data;
-}
-
-int LinkedList::front() const{
-	return at(0);
-}
-
-int& LinkedList::front(){
-	return at(0);
-}
-
-LinkedList& LinkedList::push_back(int value) {
-	if (!m_head) {
-		// First element, make it the tail
-		m_head = std::make_unique<Node>(value);
-		m_tail = m_head.get();
-	}
-	else {
-		// Already something in, to straight to the tail
-		m_tail->m_next = std::make_unique<Node>(value);
-		m_tail = m_tail->m_next.get();	
-	}
-
-	return *this;
-}
-
-LinkedList& LinkedList::pop_back(){
-	if (m_head) {
-		getLast().reset();
-		m_tail = getLast().get();
-	}
-	return *this;
-}
-
-bool LinkedList::empty() const{
-	return static_cast<bool>(m_head);
-}
+////#include "LinkedList.hpp"
+//
+// //LinkedList::Node::Node(int value) : m_data{ value }, m_next{ nullptr }{}
+//
+// LinkedList::NodePtr &LinkedList::getLast()
+// {
+// 	NodePtr *it{&m_head};
+// 	while ((*it)->m_next)
+// 	{
+// 		it = &(*it)->m_next;
+// 	}
+// 	return *it;
+// }
+//
+// LinkedList::LinkedList() : m_head{nullptr}, m_tail{nullptr} {}
+//
+// size_t LinkedList::size() const
+// {
+// 	if (!m_head)
+// 		return 0U;
+// 	size_t s{1U};
+// 	const NodePtr *node{&m_head};
+// 	while ((*node)->m_next)
+// 	{
+// 		node = &(*node)->m_next;
+// 		s++;
+// 	}
+// 	return s;
+// }
+//
+// int LinkedList::at(size_t n) const
+// {
+// 	const NodePtr *node{&m_head};
+// 	for (size_t i{0U}; i < n; i++)
+// 	{
+// 		if (!node)
+// 			throw std::out_of_range{"Index out of range."};
+// 		node = &(*node)->m_next;
+// 	}
+//
+// 	return (*node)->m_data;
+// }
+//
+// int &LinkedList::at(size_t n)
+// {
+// 	NodePtr *node{&m_head};
+// 	for (size_t i{0U}; i < n; i++)
+// 	{
+// 		if (!node)
+// 			throw std::out_of_range{"Index out of range."};
+// 		node = &(*node)->m_next;
+// 	}
+//
+// 	return (*node)->m_data;
+// }
+//
+// int LinkedList::operator[](size_t n) const
+// {
+// 	return at(n);
+// }
+//
+// int &LinkedList::operator[](size_t n)
+// {
+// 	return at(n);
+// }
+//
+// int LinkedList::back() const
+// {
+// 	return m_tail->m_data;
+// }
+//
+// int &LinkedList::back()
+// {
+// 	return m_tail->m_data;
+// }
+//
+// int LinkedList::front() const
+// {
+// 	return at(0);
+// }
+//
+// int &LinkedList::front()
+// {
+// 	return at(0);
+// }
+//
+// LinkedList &LinkedList::push_back(int value)
+// {
+// 	if (!m_head)
+// 	{
+// 		// First element, make it the tail
+// 		m_head = std::make_unique<Node>(value);
+// 		m_tail = m_head.get();
+// 	}
+// 	else
+// 	{
+// 		// Already something in, to straight to the tail
+// 		m_tail->m_next = std::make_unique<Node>(value);
+// 		m_tail = m_tail->m_next.get();
+// 	}
+//
+// 	return *this;
+// }
+//
+// LinkedList &LinkedList::pop_back()
+// {
+// 	if (m_head)
+// 	{
+// 		getLast().reset();
+// 		m_tail = getLast().get();
+// 	}
+// 	return *this;
+// }
+//
+// bool LinkedList::empty() const
+// {
+// 	return static_cast<bool>(m_head);
+// }
--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,67 +1,177 @@
 #ifndef LINKED_LIST_HPP
 #define LINKED_LIST_HPP
 
-#include <stdexcept>
+#include <exception>
 #include <memory>
 
-class LinkedList {
+template <typename T>
+class LinkedList
+{
 	struct Node;
 	using NodePtr = std::unique_ptr<Node>;
-	
-	struct Node {
-		int m_data;
+
+	struct Node
+	{
+		T m_data;
 		NodePtr m_next;
 
-		Node(int value);
+		Node(const T &value);
 	};
 
 	NodePtr m_head;
-	Node* m_tail;
+	Node *m_tail;
 
 	// Retrieves the last node of the list
 	// Complexity: O(n)
-	NodePtr& getLast();
+	NodePtr &getLast();
 
 public:
 	LinkedList();
-	
+
 	// Returns the size of the list
 	// Complexity: O(n)
 	size_t size() const;
 
 	// Rerturns the value at position n
 	// Complexity: O(n)
-	int at(size_t n) const;
-	int& at(size_t n);
-	
+	T at(size_t n) const;
+	T& at(size_t n);
+
 	// Syntax wrapper for Linkedlist::at()
-	int operator[](size_t n) const;
-	int& operator[](size_t n);
+	T operator[](size_t n) const;
+	T &operator[](size_t n);
 
 	// Returns the value at the tail of the list
 	// Complexity: O(1)
-	int back() const;
-	int& back();
+	T back() const;
+	T& back();
 
 	// Returns the value at the head of the list
 	// Complexity: O(1)
-	int front() const;
-	int& front();
+	T front() const;
+	T &front();
 
 	// Inserts a copy of the value at the tail
 	// Complexity: O(n)
-	LinkedList& push_back(int value);
-	
+	LinkedList &push_back(T value);
+
 	// Discards the value at the tail
 	// Complexity: O(1)
-	LinkedList& pop_back();
+	LinkedList &pop_back();
 
 	// Whether or not the list is empty
 	// Conplexity: O(1)
 	bool empty() const;
-	
 };
 
+template<typename T>
+inline LinkedList<T>::Node::Node(const T& value) : m_data{ value }, m_next{ nullptr } {}
 
+template<typename T>
+inline LinkedList<T>::NodePtr& LinkedList<T>::getLast(){
+	NodePtr* it{ &m_head };
+	while ((*it)->m_next){
+		it = &(*it)->m_next;
+	}
+	return *it;
+}
+
+template<typename T>
+inline LinkedList<T>::LinkedList() : m_head{ nullptr }, m_tail{ nullptr } {}
+
+template<typename T>
+inline size_t LinkedList<T>::size() const {
+ 	if (!m_head) 
+		return 0U;
+ 	size_t s{1U};
+ 	const NodePtr *node{&m_head};
+ 	while ((*node)->m_next){
+ 		node = &(*node)->m_next;
+ 		s++;
+ 	}
+ 	return s;
+ }
+
+template<typename T>
+inline T LinkedList<T>::at(size_t n) const {
+	const NodePtr* node{ &m_head };
+	for (size_t i{ 0U }; i < n; i++){
+		if (!node)
+			throw std::out_of_range{ "Index out of range." };
+		node = &(*node)->m_next;
+	}
+}
+
+template<typename T>
+inline T& LinkedList<T>::at(size_t n) {
+ 	NodePtr *node{&m_head};
+ 	for (size_t i{0U}; i < n; i++){
+ 		if (!node)
+ 			throw std::out_of_range{"Index out of range."};
+ 		node = &(*node)->m_next;
+ 	}
+
+ 	return (*node)->m_data;
+}
+
+template<typename T>
+inline T LinkedList<T>::operator[](size_t n) const{
+	return at(n);
+}
+
+template<typename T>
+inline T& LinkedList<T>::operator[](size_t n){
+	return at(n);
+}
+
+template<typename T>
+inline T LinkedList<T>::back() const{
+	return m_tail->m_data;
+}
+
+template<typename T>
+inline T& LinkedList<T>::back(){
+	return m_tail->m_data;
+}
+
+template<typename T>
+inline T LinkedList<T>::front() const{
+	return at(0);
+}
+
+template<typename T>
+inline T& LinkedList<T>::front(){
+	return at(0);
+}
+
+template<typename T>
+inline LinkedList<T>& LinkedList<T>::push_back(T value){
+ 	if (!m_head){
+ 		// First element, make it the tail
+ 		m_head = std::make_unique<Node>(value);
+ 		m_tail = m_head.get();
+ 	}
+ 	else{
+ 		// Already something in, to straight to the tail
+ 		m_tail->m_next = std::make_unique<Node>(value);
+ 		m_tail = m_tail->m_next.get();
+ 	}
+
+ 	return *this;
+ }
+
+template<typename T>
+inline LinkedList<T>& LinkedList<T>::pop_back(){
+ 	if (m_head){
+ 		getLast().reset();
+ 		m_tail = getLast().get();
+ 	}
+ 	return *this;
+}
+
+template<typename T>
+inline bool LinkedList<T>::empty() const{
+	return static_cast<bool>(m_head);
+}
 
 #endif // !LINKED_LIST_HPP
